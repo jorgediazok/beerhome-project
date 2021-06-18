8a35633aa81283432f32335fd632729bcb35ad92
Jorge <jorgediazok@gmail.com>
2021-06-18 02:07:53 -0300

    Added navbar and hero. Not completed yet, needs more styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //Pages for routing
 import Home from './pages/Home';
+import About from './pages/About';
 import Signup from './pages/Signup';
-import Products from './pages/Products';
+import Shop from './pages/Shop';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
@@ -21,8 +22,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/products" component={Products} />
-          <Route path="/products/:id" component={Product} />
+          <Route path="/nosotros" component={About} />
+          <Route path="/tienda" component={Shop} />
+          <Route path="/tienda/:id" component={Product} />
           <Route path="/signup" component={Signup} />
           <Route path="/cart" component={Cart} />
           <Route path="/checkout" component={Checkout} />
--- client/src/pages/Products.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Products = () => {
-  return <div>This is the products Page</div>;
-};
-
-export default Products;
