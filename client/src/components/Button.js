47d8032a7f7dbade0dd699fba25fa0ca338419f9
Jorge <jorgediazok@gmail.com>
2021-06-22 22:11:00 -0300

    added a comment to test something

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Button.scss';
+//THis is a testing message. Must be removed later.
 
 const Button = () => {
   return (
