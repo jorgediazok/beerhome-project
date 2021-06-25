4d65f98f494c611fa150dee3a33b99e47c3db519
Christian <christianmxfantin@gmail.com>
2021-06-25 20:00:45 -0300

    I'm the change

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Signup = () => {
   return <div>This is the signup/login page</div>;
 };
 
 export default Signup;
+
+//Hola, soy el cambio
