7935a99ef3306ce3550cd806991d27dab2d590b8
Jorge <jorgediazok@gmail.com>
2021-06-28 02:16:59 -0300

    added api connection

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Button.scss';
-
+//comment
 const Button = () => {
   return (
     <Link to="/signup">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+//API
+import Client from '../api/api';
+
+//COMPONENTS
 import Hero from '../components/Hero';
+import OurProducts from '../components/HomeSections/OurProducts';
 
 const Home = () => {
+  const [beers, setBeers] = useState([]);
+
+  //CALLING THE API
+  const getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: 'beerHouseProject',
+      });
+      const fetchedBeers = await response.items;
+      setBeers(fetchedBeers);
+      console.log(beers);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <Hero />
