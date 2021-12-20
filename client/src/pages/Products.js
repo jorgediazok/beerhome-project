89415f9a4a657452902d57f38dabcccbb01118fb
Jorge <jorgediazok@gmail.com>
2021-12-20 00:03:22 -0300

    styled the mobile version of detail page

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ProductCard from '../components/ProductCard';
 import '../styles/Products.scss';
 
-//NECESITO CARDS PARA MI CATALOGO, UNA UI, CUANDO CLICKEO EN ESAS CARDS, ME
-//LEVA A PRODUCT SINGLE. PERO, de mientras, no me sirve aca. CREO OTRA CARD.
-
 const Products = ({ beers }) => {
   return (
     <div className='products__container'>
