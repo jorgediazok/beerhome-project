019db0c53a097cbd629252e4babd8ce61ac6ce67
Jorge <jorgediazok@gmail.com>
2022-01-06 01:47:03 -0300

    fixing bug in routing

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('client/build/'));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 //Using Route
