77f03d02cc9d2d40248f94f005f026013e68c32e
Jorge <jorgediazok@gmail.com>
2022-01-05 21:09:47 -0300

    setting app to be deployed in heroku

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static('client/build/'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 //Using Route
 app.use('/user', userRoutes);
